7903133a8b91456b58d659da81fba3711dbf998a
bernardo-333 <bernardinho.freitas11@gmail.com>
2025-11-14 11:11:22 -0300

    feat: created initial structure Api

---
+++ src/main/java/com/bernardo/ceplistapi/CeplistapiApplication.java
@@ -0,0 +1,13 @@
+package com.bernardo.ceplistapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeplistapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CeplistapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bernardo/ceplistapi/CeplistapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bernardo.ceplistapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CeplistapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
